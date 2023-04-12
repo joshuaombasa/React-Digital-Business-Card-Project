a6f602c780fb3bc1f1bf316c4f40ab42df6d577d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-12 23:14:35 +0300

    added intial component script

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+              <img className='user-image' src="src/assets/joe.jpeg" alt="Joshua ombasa smiling at the camera" />
+        </header>
+    )
+}
+
+export default Header
